a30db92ed7950c6159ecdae577820a00be943728
Tim Perkins <perkins@adobe.com>
2016-03-29 22:54:24 -0600

    Audio player enhancements

--- public/js/timaud.js
+++ public/js/timaud.js
@@ -30,18 +30,23 @@
 			var $sliderWrap = $('<div class="tim-aud-slider-wrap"></div>').appendTo($title);
 			var $slider = $('<div class="tim-aud-slider"></div>').appendTo($sliderWrap);
 			var $sliderBar = $('<div class="tim-aud-slider-bar"></div>').appendTo($slider);
+			var $sliderHandle = $('<div class="tim-aud-slider-handle"></div>').appendTo($sliderBar);
 			var $timeRemaining = $('<p class="tim-aud-time-remaining"></p>').appendTo($slider);
 
-			var $audio = $('<audio src="' + opts.src + '"></audio>').appendTo($el);
+			var $audio = $('<audio src="' + opts.src + '" preload="none"></audio>').appendTo($el);
 			var audio = $audio[0];
 
 			$playButton.on('click', play);
 			$pauseButton.on('click', pause);
+			$sliderHandle.on('mousedown', startHandleMove);
+			$sliderBar.on('mousedown', moveHandle);
+			$audio.on('ended', onEnded);
+			var handleIsMoving = false;
 			var interval;
 
 			function play() {
 				changeState(PLAY_STATE);
-				interval = setInterval(updateTime, 1000);
+				interval = setInterval(updateTime, 50);
 				updateTime();
 				audio.play();
 
@@ -56,8 +61,71 @@
 				_pauseCurrentAudio = function(){};
 			}
 
+			function onEnded(e) {
+				changeState(INITIAL_STATE);
+				clearInterval(interval);
+				_pauseCurrentAudio = function(){};
+				audio.currentTime = 0;
+			}
+
 			function updateTime() {
-				$timeRemaining.text(formatTime(audio.duration - audio.currentTime));
+				var pos = Math.min(99, (100 * audio.currentTime / audio.duration).toFixed(1));
+				if (updateTime.pos !== pos && !handleIsMoving) {
+					updateTime.pos = pos;
+					$sliderHandle.css('left', pos + '%');
+				}
+
+				var timeRemaining = formatTime(audio.duration - audio.currentTime);
+				if (updateTime.timeRemaining != timeRemaining) {
+					updateTime.timeRemaining = timeRemaining;
+					$timeRemaining.text(timeRemaining);
+				}
+				updateBufferIndicators();
+			}
+
+			function startHandleMove(e) {
+				e.stopPropagation();
+				handleIsMoving = true;
+				$(window).on('mousemove', handleMove);
+				$(window).on('mouseup', stopHandleMove);
+			}
+
+			function handleMove(e) {
+				var xPos = e.pageX - $sliderBar.offset().left;
+				xPos = Math.max(0, xPos);
+				xPos = Math.min($sliderBar.width()-10, xPos);
+				$sliderHandle.css('left', (xPos - 5) + 'px');
+			}
+
+			function stopHandleMove(e) {
+				var pos = parseInt($sliderHandle.css('left')) / $sliderBar.width();
+				handleIsMoving = false;
+				$(window).off('mousemove', handleMove);
+				$(window).off('mouseup', stopHandleMove);
+				audio.currentTime = parseInt(audio.duration * pos);
+			}
+
+			function moveHandle(e) {
+				handleMove(e);
+				stopHandleMove(e);
+			}
+
+			var bufferIndicators = [];
+			function updateBufferIndicators() {
+				for (var i=bufferIndicators.length; i<audio.buffered.length; i++) {
+					// Create new buffer indicators
+					var $b = $('<div class="tim-aud-buffer-indicator"></div>').appendTo($sliderBar);
+					bufferIndicators.push($b);
+				}
+				bufferIndicators.length = audio.buffered.length;
+				for (var i=0; i<audio.buffered.length; i++) {
+					var start = 100 * audio.buffered.start(i) / audio.duration;
+					var end = Math.min(100, 100 * audio.buffered.end(i) / audio.duration);
+					bufferIndicators[i].css({
+						left: start + '%',
+						width: (end - start) + '%'
+					});
+				}
 			}
 
 			function changeState(toState) {
@@ -65,21 +133,24 @@
 				$el.addClass(toState);
 
 				if (toState == PLAY_STATE) {
-					$sliderWrap.stop(true, true).fadeIn();
+					$sliderWrap.stop(true, true).fadeIn(500);
 				} else {
-					$sliderWrap.stop(true, true).fadeOut();
+					$sliderWrap.stop(true, true).fadeOut(500);
 				}
 			}
 			changeState(INITIAL_STATE);
 		}
 
 		function formatTime(sec) {
-			var hours = parseInt(sec/3600);
-			var minutes = parseInt(sec/60);
-			var seconds = parseInt(sec%60);
+			var hours = parseInt(sec/3600) || 0;
+			var minutes = parseInt(sec/60) || 0;
+			var seconds = parseInt(sec%60) || 0;
 			var time = [minutes, seconds];
 			if (hours) {
 				time.unshift(hours);
+			}
+			if (hours === 0 && minutes === 0 && seconds === 0) {
+				return '';
 			}
 			time = time.map(function(o) {
 				o = o.toString();
@@ -89,6 +160,19 @@
 				return o;
 			});
 			return time.join(':');
+		}
+
+		function debounce(fn, delay) {
+			var timeoutId;
+			return function(e) {
+				if (timeoutId) {
+					clearTimeout(timeoutId);
+				}
+				timeoutId = setTimeout(function() {
+					fn.call(fn, e);
+					timeoutId = null;
+				}, delay);
+			};
 		}
 	};
 })(jQuery);
