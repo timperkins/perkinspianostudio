27ceb274f6e7caa8d87d827c60839743133c06d7
Tim Perkins <perkins@adobe.com>
2016-04-16 18:37:41 -0600

    Update video player

--- public/js/timvid.js
+++ public/js/timvid.js
@@ -5,7 +5,6 @@
 		var $poster = this.find('[data-timvid-poster]');
 		var $controls = this.find('[data-timvid-controls]');
 		var aspectRatio = $poster.height()/$poster.width();
-		var $videoPlayer;
 
 		// Load YouTube API
 		var tag = document.createElement('script');
@@ -16,29 +15,20 @@
 		var player;
 		window.onYouTubeIframeAPIReady = function() {
 			player = new YT.Player($video.get(0), {
-				height: '315',
-				width: '560',
-				videoId: 'DQ1FveZJCZA',
 				events: {
 					onReady: onPlayerReady
 				},
-				playerVars: {
-					autoHide: 1,
-					modestbranding: 1,
-					showinfo: 0
-				}
 			});
 		};
 
 		function onPlayerReady() {
-			$videoPlayer = $section.find("iframe");
 			initFluidWidth();
 			$controls.on('click', onPosterClick);
 			$controls.addClass('ready');
 		}
 
 		function onPosterClick() {
-			$videoPlayer.fadeIn(200);
+			$video.fadeIn(200);
 			player.playVideo();
 			$controls.off('click', onPosterClick);
 		}
@@ -51,7 +41,7 @@
 		function resizeStuff() {
 			var newWidth = $section.width();
 			var newHeight = newWidth * aspectRatio;
-			$videoPlayer.width(newWidth).height(newHeight);
+			$video.width(newWidth).height(newHeight);
 		}
 
 		function debounce(fn, delay) {
